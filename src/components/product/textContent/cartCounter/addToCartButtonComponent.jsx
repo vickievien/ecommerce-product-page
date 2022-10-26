6b1f51fcf38a74ce6f84ae5517e3870f8f20b93e
Vickie Vien <vickievien@gmail.com>
2022-10-26 12:28:02 -0700

    style: updated classname to be reusable

--- src/components/product/textContent/cartCounter/addToCartButtonComponent.jsx
+++ src/components/product/textContent/cartCounter/addToCartButtonComponent.jsx
@@ -1,7 +1,7 @@
 const AddToCartButtonComponent = (props) => {
 
     return (
-        <a href="#" className="button add-to-cart" onClick={props.addToCartButton}>
+        <a href="#" className="button button-primary add-to-cart" onClick={props.addToCartButton}>
             <img src="/images/icon-cart-white.svg" alt="cart" className="cart-icon"/>
             <p>Add to cart</p>
         </a>
