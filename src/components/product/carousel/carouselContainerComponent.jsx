d77ea0bff1fbe1f1cc86bd25109e95529f6770ad
Vickie Vien <vickievien@gmail.com>
2022-10-26 22:23:47 -0700

    style: updated carousel thumbnail styling to match design added styles for tablet view

--- src/components/product/carousel/carouselContainerComponent.jsx
+++ src/components/product/carousel/carouselContainerComponent.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const CarouselContainerComponent = () => {
     const array = [1,2,3,4];
     const images = array.map(x => `image-product-${x}.jpg`);
-    const imageThumbnails = array.map(x => `image-product-${x}-thumbnail.jpg`);
+    const imageSrcThumbnails = array.map(x => `image-product-${x}-thumbnail.jpg`);
 
     let [index, setIndex] = useState(0);
 
@@ -17,6 +17,10 @@ const CarouselContainerComponent = () => {
         setIndex(index > 0 ? index - 1 : numOfImages)
     }
 
+    const imageThumbnails = imageSrcThumbnails.map (x => 
+        <img src={`./images/${x}`} alt="product-image" />
+        )
+
     return (
         <section className="carousel-container">
             <img className="carousel-image" src={`./images/${images[index]}`} alt="image-product" />
@@ -27,6 +31,9 @@ const CarouselContainerComponent = () => {
                 <div className="arrow-wrapper" onClick={nextSlide}>
                     <img className="arrow" src="./images/icon-next.svg" alt="next arrow" />
                 </div>
+            </div>
+            <div className="carousel-thumbnail-container">
+                {imageThumbnails}
             </div>
         </section>
     )
