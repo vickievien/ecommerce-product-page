d5f0b538de15d83ac7dd5d2d8781e6699c8180ec
Vickie Vien <vickievien@gmail.com>
2022-10-26 21:05:45 -0700

    style: updated cart modal style to match design

--- src/components/cartModal/cartModalComponent.jsx
+++ src/components/cartModal/cartModalComponent.jsx
@@ -2,19 +2,22 @@ const CartModalComponent = () => {
 
     return (
         <section className="cart-modal-container">
-            <h3 className="cart-modal-title">Cart</h3>
-            <ul className="cart-list-container">
-                <li className="cart-list-item-container">
-                    <a href="#" className="cart-list-item-image-link">
-                        <img src="./images/image-product-1-thumbnail.jpg" alt="product-image" />
-                    </a>
-                    <div className="cart-list-item-detail-container">
-                        <p>Fall Limited Edition Sneakers</p>
-                        <p>CALCULATE PRICE</p>
-                    </div>
-                </li>
-            </ul>
-            <a href="#" className="button-primary">Checkout</a>
+            <div className="cart-modal-info-container">
+                <h3 className="cart-modal-title">Cart</h3>
+                <ul className="cart-list-container">
+                    <li className="cart-list-item-container">
+                        <a href="#" className="cart-list-item-image-link">
+                            <img src="./images/image-product-1-thumbnail.jpg" alt="product-image" />
+                        </a>
+                        <div className="cart-list-item-detail-container">
+                            <p>Fall Limited Edition Sneakers</p>
+                            <p>$5.55 x 3 $333.55</p>
+                        </div>
+                        <img src="./images/icon-delete.svg" className="icon-delete"/>
+                    </li>
+                </ul>
+                <a href="#" className="button-primary checkout ">Checkout</a>
+            </div>
         </section>
     )
 }
