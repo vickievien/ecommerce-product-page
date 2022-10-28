aeff66efc84b4aebdbe1e991ed5e650e0ff80ce1
Vickie Vien <vickievien@gmail.com>
2022-10-28 13:55:17 -0700

    feat: added lightbox component and state variables to product container component

--- src/components/product/productContainerComponent.jsx
+++ src/components/product/productContainerComponent.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+import LightBoxContainerComponent from "../lightBox/lightboxContainerComponent";
 import CarouselContainerComponent from "./carousel/carouselContainerComponent";
 import TextContentContainerComponent from "./textContent/textContentContainer";
 
 const ProductContainerComponent = (props) => {
+    const [lightbox, setLightbox] = useState(true);
+
+    const toggleLightBox = () => {
+        setLightbox(!lightbox);
+      }
 
     return (
         <section className="product-container">
-            <CarouselContainerComponent />
+            <CarouselContainerComponent lightbox={lightbox} toggleLightBox={toggleLightBox}/>
             <TextContentContainerComponent  addItemToCart={props.addItemToCart} deleteItemToCart={props.deleteItemToCart} counter={props.counter} addToCartButton={props.addToCartButton} productPrice={props.productPrice} discount={props.discount} discountedPrice={props.discountedPrice} />
+            {lightbox ? 
+                <LightBoxContainerComponent lightbox={lightbox}  className="lightbox" toggleLightBox={toggleLightBox}/>
+            : ""}
         </section>
     )
 }
