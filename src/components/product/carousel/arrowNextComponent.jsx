59b69157e1fe59ebf50f281e786f9c64ae6d699a
Vickie Vien <vickievien@gmail.com>
2022-10-28 16:20:07 -0700

    :wheelchair: a11y: add role and aria label in svg components

--- src/components/product/carousel/arrowNextComponent.jsx
+++ src/components/product/carousel/arrowNextComponent.jsx
@@ -1,7 +1,7 @@
 const ArrowNextComponent = () => {
 
     return (
-        <svg className="arrow arrow-next" width="13" height="18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" stroke-width="3" fill="none" fill-rule="evenodd"/></svg>
+        <svg role="graphics-symbol" aria-labelledby="next-arrow" className="arrow arrow-next" width="13" height="18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" strokeWidth="3" fill="none" fillRule="evenodd"/></svg>
     )
 }
 
--- src/components/product/carousel/arrowPreviousComponent.jsx
+++ src/components/product/carousel/arrowPreviousComponent.jsx
@@ -1,7 +1,7 @@
 const ArrowPreviousComponent = () => {
 
     return (
-        <svg className="arrow arrow-previous" width="12" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M11 1 3 9l8 8" stroke-width="3" fill="none" fill-rule="evenodd"/></svg>
+        <svg role="graphics-symbol" aria-labelledby="previous-arrow" className="arrow arrow-previous" width="12" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M11 1 3 9l8 8" strokeWidth="3" fill="none" fillRule="evenodd"/></svg>
     )
 }
 
