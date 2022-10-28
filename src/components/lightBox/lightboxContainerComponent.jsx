74f91d48b0d3815fd1519082f130760a913d3714
Vickie Vien <vickievien@gmail.com>
2022-10-28 14:18:08 -0700

    chore: created image close component added component to parent component

--- src/components/lightBox/lightboxContainerComponent.jsx
+++ src/components/lightBox/lightboxContainerComponent.jsx
@@ -1,8 +1,12 @@
-const LightBoxContainerComponent = () => {
+import CarouselContainerComponent from "../product/carousel/carouselContainerComponent";
+import ImageCloseComponent from "./imageCloseComponent";
+
+const LightBoxContainerComponent = (props) => {
 
     return (
         <section className="lightbox-container">
-
+            <ImageCloseComponent toggleLightBox={props.toggleLightBox}/>
+            <CarouselContainerComponent lightbox={props.lightbox} className="lightbox"/>
         </section>
     )
 }
