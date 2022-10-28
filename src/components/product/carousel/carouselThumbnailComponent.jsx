8e259e5aa4045cdc69f68b75e25889e9215266d6
Vickie Vien <vickievien@gmail.com>
2022-10-28 15:37:00 -0700

    chore: added div wrapper to carousel thumbnail images to be able to apply pseudo element styles to match Figma design Moved focus classname and onClick function onto new wrapper to keep functionality

--- src/components/product/carousel/carouselThumbnailComponent.jsx
+++ src/components/product/carousel/carouselThumbnailComponent.jsx
@@ -1,6 +1,8 @@
 const CarouselThumbnailComponent = (props) => {
     const imageThumbnails = props.imageSrcThumbnails.map (x => 
-        <img src={`./images/${x}-thumbnail.jpg`} alt="product-image" key={`thumbnail-${x}`} className={`carousel-thumbnail-image ${props.imageSrcThumbnails.indexOf(x) + 1 === props.imageNum ? 'focus' : ''}`} onClick={() => props.changeThumbnail(x)}/>
+        <div className={`carousel-thumbnail-image-wrapper ${props.imageSrcThumbnails.indexOf(x) + 1 === props.imageNum ? 'focus' : ''}`} onClick={() => props.changeThumbnail(x)}>
+            <img src={`./images/${x}-thumbnail.jpg`} alt="product-image" key={`thumbnail-${x}`} className="carousel-thumbnail-image" />
+        </div>
     )
 
     return (
